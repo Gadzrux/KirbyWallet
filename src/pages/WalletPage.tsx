eb7269b63e5ba6eea212f0a8db147daae96a0766
gadzrux <krishnavaghosh23@gmail.com>
2024-08-17 12:03:07 +0530

    update number of sol and send functionality

--- src/pages/WalletPage.tsx
+++ src/pages/WalletPage.tsx
@@ -1,18 +1,23 @@
 import { useState, useEffect } from 'react';
 import { Buffer } from 'buffer';
-import { Keypair} from "@solana/web3.js";
-import { HDNodeWallet, encodeBase58  } from "ethers";
+import { Keypair, Connection, LAMPORTS_PER_SOL, Transaction, SystemProgram } from "@solana/web3.js";
+import { HDNodeWallet, encodeBase58, computeAddress } from "ethers";
 import { mnemonicToSeed } from 'bip39';
 import { derivePath } from 'ed25519-hd-key';
 import nacl from 'tweetnacl';
+import axios from 'axios';
 
 function WalletPage(): JSX.Element {
-
-  const [ethAccounts, setethAccounts] = useState(0);
-  const [solAccounts, setsolAccounts] = useState(0);
-  const [ethKeys, setEthKeys] = useState<{ privateKey: string; publicKey: string; }[]>([]);
-  const [solKeys, setSolKeys] = useState<{ privateKey: string; publicKey: string; }[]>([]);
+  const [ethAccounts, setethAccounts] = useState<number>(0);
+  const [solAccounts, setsolAccounts] = useState<number>(0);
+  const [ethKeys, setEthKeys] = useState<{ privateKey: string; publicKey: string; ethTokens: number;}[]>([]);
+  const [solKeys, setSolKeys] = useState<{ privateKey: string; publicKey: string; solTokens: number;}[]>([]);
   const [seed, setSeed] = useState<Uint8Array>();
+
+  const [sendSOLAmount, setSendSOLAmount] = useState<number>(0);
+  const [sendSOLAddress, setSendSOLAddress] = useState<string>("");
+  const [sendETHAmount, setSendETHAmount] = useState<number>(0);
+  const [sendETHAddress, setSendETHAddress] = useState<string>("");
 
   useEffect(() => {
     const mnemonics = localStorage.getItem("mnemonics");
@@ -23,32 +28,55 @@ function WalletPage(): JSX.Element {
     }
   }, []);
 
-  useEffect(() => {
+  const fetchEthData = async () => {
     if (seed) {
       const hdNode = HDNodeWallet.fromSeed(seed);
       const path = `m/44'/60'/${ethAccounts}'/0`;
       const wallet = hdNode.derivePath(path);
+      const data = await axios.post("https://eth-holesky.g.alchemy.com/v2/atNVG8-veYm43Ng99fSUIFOixbOyPPZe", {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "eth_getBalance",
+        "params": [`${computeAddress(wallet.publicKey)}`, "latest"]
+      });
+      const ethTokensHex = data.data.result;
+      const ethTokens = Number(ethTokensHex) / Math.pow(10, 18);
       setEthKeys([...ethKeys, {
         privateKey: wallet.privateKey,
-        publicKey: wallet.publicKey,
+        publicKey: computeAddress(wallet.publicKey),
+        ethTokens,
       }]);
     }
-  }, [ethAccounts]);
+  };
 
-  useEffect(() => {
-    if(seed) {
-      // const solSeed = seed.slice(0, 32);
+  const fetchSolData = async () => {
+    if (seed) {
       const path = `m/44'/501'/${solAccounts}'/0'`;
       const hexSeed = Buffer.from(seed).toString('hex');
       const derivedSeed = derivePath(path, hexSeed).key;
       const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
       const keypair = Keypair.fromSecretKey(secret);
-      console.log(typeof(keypair.publicKey));
+      const data = await axios.post("https://solana-devnet.g.alchemy.com/v2/atNVG8-veYm43Ng99fSUIFOixbOyPPZe", {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "getBalance",
+        "params": [`${keypair.publicKey.toString()}`]
+      });
+      const solTokens = data.data.result.value / Math.pow(10, 9);
       setSolKeys([...solKeys, {
-      privateKey: encodeBase58(keypair.secretKey),
-      publicKey: keypair.publicKey.toString(),
-    }]);
+        privateKey: encodeBase58(keypair.secretKey),
+        publicKey: keypair.publicKey.toString(),
+        solTokens,
+      }]);
     }
+  };
+
+  useEffect(() => {
+    fetchEthData();
+  }, [ethAccounts]);
+
+  useEffect(() => {
+    fetchSolData();
   }, [solAccounts]);
 
   const addSolAcc = () => {
@@ -57,6 +85,13 @@ function WalletPage(): JSX.Element {
 
   const addEthAcc = () => {
     setethAccounts(ethAccounts + 1);
+  };
+
+  const sendETH = async (index: number) => {
+
+  };
+
+  const sendSOL = async (index: number) => {
   };
 
   return (
@@ -81,18 +116,33 @@ function WalletPage(): JSX.Element {
           <div className='text-white font-semibold font-sans m-10 text-4xl'>SOL WALLET</div>
           {Array.from({ length: solAccounts }).map((_, index) => (
             solKeys[index] && (
-              <div id="m-2 show-sol-accounts" key={Math.random()}>
+              <div id="m-2 show-sol-accounts p-2" key={Math.random()}>
                 <div className="collapse bg-base-200">
                   <input type="checkbox" />
-                  <div className="collapse-title text-xl font-medium">Account {index + 1}</div>
-                  {/* Show amount of SOL */}
-                  <div className="collapse-content">
-                    <p className=' bg-green-500 rounded-3xl p-3 text-white'>Public Key:</p>
-                    <div className='overflow-x-auto w-72 border border-white m-2 p-2 text-white font-bold rounded-md'>{solKeys[index].publicKey}</div>
-                    <p className='bg-red-500 rounded-3xl p-3 text-white'>Private Key:</p>
-                    <div className='overflow-x-auto w-72 border border-white m-2 p-2 text-white font-bold rounded-md'>{solKeys[index].privateKey}</div>
+                  <div className="flex flex-row justify-between collapse-title text-xl font-medium">
+                    <div>Account: {index + 1}</div>
+                    <div>SOL: {solKeys[index].solTokens}</div>
                   </div>
-                  {/* Implement Send SOL button */}
+                  <div className="collapse-content">
+                    <p className=' text-green-500 p-3'>Public Key:</p>
+                    <div className='overflow-x-auto w-72 border border-white m-2 p-2 text-white font-bold rounded-md'>
+                      {solKeys[index].publicKey}
+                    </div>
+                    <p className='text-red-500 p-3'>Private Key:</p>
+                    <div className='overflow-x-auto w-72 border border-white m-2 p-2 text-white font-bold rounded-md'>
+                      {solKeys[index].privateKey}
+                    </div>
+                  </div>
+                  <input type="number"
+                    className="m-2 input input-bordered w-full max-w-xs"
+                    value={sendSOLAmount}
+                    onChange={(e) => setSendSOLAmount(Number(e.target.value))} />
+                  <input type="text"
+                    placeholder="Enter Wallet Address"
+                    className="m-2 input input-bordered w-full max-w-xs"
+                    value={sendSOLAddress}
+                    onChange={(e) => setSendSOLAddress(e.target.value)} />
+                  <button onClick={() => sendSOL(index)} className="btn btn-active">SEND SOL</button>
                 </div>
               </div>
             )
@@ -112,15 +162,34 @@ function WalletPage(): JSX.Element {
               <div key={Math.random()}>
                 <div className="m-2 collapse bg-base-200">
                   <input type="checkbox" />
-                  <div className="collapse-title text-xl font-medium">Account {index + 1}</div>
-                  {/* Show amount of ETH */}
-                  <div className="collapse-content">
-                    <p className=' bg-green-500 rounded-3xl p-3 text-white'>Public Key:</p>
-                    <div className='overflow-x-auto w-72 border border-white m-2 p-2 text-white font-bold rounded-md'>{ethKeys[index].publicKey}</div>
-                    <p className='bg-red-500 rounded-3xl p-3 text-white'>Private Key:</p>
-                    <div className='overflow-x-auto w-72 border border-white m-2 p-2 text-white font-bold rounded-md'>{ethKeys[index].privateKey}</div>
+                  <div className="flex flex-row justify-between collapse-title text-xl font-medium">
+                    <div>
+                      Account {index + 1}
+                    </div>
+                    <div>
+                      ETH: {ethKeys[index].ethTokens}
+                    </div>
                   </div>
-                  {/* Implement Send ETH button */}
+                  <div className="collapse-content">
+                    <p className=' text-green-500 p-3'>Public Key:</p>
+                    <div className='overflow-x-auto w-72 border border-white m-2 p-2 text-white font-bold rounded-md'>
+                      {ethKeys[index].publicKey}
+                    </div>
+                    <p className=' text-red-500 p-3'>Private Key:</p>
+                    <div className='overflow-x-auto w-72 border border-white m-2 p-2 text-white font-bold rounded-md'>
+                      {ethKeys[index].privateKey}
+                    </div>
+                  </div>
+                  <input type="number"
+                    className="m-2 input input-bordered w-full max-w-xs"
+                    value={sendETHAmount}
+                    onChange={(e) => setSendETHAmount(Number(e.target.value))} />
+                  <input type="text"
+                    placeholder="Enter Wallet Address"
+                    className="m-2 input input-bordered w-full max-w-xs"
+                    value={sendETHAddress}
+                    onChange={(e) => setSendETHAddress(e.target.value)} />
+                  <button onClick={() => sendETH(index)} className="btn btn-active">SEND ETH</button>
                 </div>
               </div>
             )
