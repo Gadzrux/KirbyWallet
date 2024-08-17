f399489de3d94934f0dcee59c413992ec74a79bd
gadzrux <krishnavaghosh23@gmail.com>
2024-08-17 17:38:28 +0530

    added functionality to send and recieve eth

--- src/pages/WalletPage.tsx
+++ src/pages/WalletPage.tsx
@@ -1,35 +1,56 @@
 import { useState, useEffect } from 'react';
 import { Buffer } from 'buffer';
-import { Keypair} from "@solana/web3.js";
-import { HDNodeWallet, encodeBase58, computeAddress } from "ethers";
+import { clusterApiUrl, 
+  Connection, 
+  Keypair, 
+  LAMPORTS_PER_SOL,
+   PublicKey, 
+   sendAndConfirmTransaction, 
+   SystemProgram, 
+   Transaction} from "@solana/web3.js";
+import { HDNodeWallet, computeAddress, JsonRpcProvider, parseEther, parseUnits } from "ethers";
 import { mnemonicToSeed } from 'bip39';
 import { derivePath } from 'ed25519-hd-key';
 import nacl from 'tweetnacl';
 import axios from 'axios';
+import bs58 from 'bs58'
+import { Wallet } from 'ethers';
+
+interface ETHAccount {
+  privateKey: string; 
+  publicKey: string; 
+  ethTokens: number; 
+  sendETHAmount: number; 
+  sendETHAddress: string;
+}
+
+interface SOLAccount {
+  privateKey: string;
+  publicKey: string;
+  solTokens: number;
+  sendSOLAmount: number;
+  sendSOLAddress: string;
+}
 
 function WalletPage(): JSX.Element {
+
   const [ethAccounts, setethAccounts] = useState<number>(0);
   const [solAccounts, setsolAccounts] = useState<number>(0);
-  const [ethKeys, setEthKeys] = useState<{ privateKey: string; publicKey: string; ethTokens: number;}[]>([]);
-  const [solKeys, setSolKeys] = useState<{ privateKey: string; publicKey: string; solTokens: number;}[]>([]);
+  const [ethKeys, setEthKeys] = useState<ETHAccount[]>([]);
+  const [solKeys, setSolKeys] = useState<SOLAccount[]>([]);
   const [seed, setSeed] = useState<Uint8Array>();
 
-  const [sendSOLAmount, setSendSOLAmount] = useState<number>(0);
-  const [sendSOLAddress, setSendSOLAddress] = useState<string>("");
-  const [sendETHAmount, setSendETHAmount] = useState<number>(0);
-  const [sendETHAddress, setSendETHAddress] = useState<string>("");
-
   useEffect(() => {
-    const mnemonics = localStorage.getItem("mnemonics");
-    if (mnemonics) {
-      mnemonicToSeed(mnemonics).then((seed) => {
+      const mnemonics = localStorage.getItem("mnemonics");
+      if (mnemonics) {
+        mnemonicToSeed(mnemonics).then((seed) => {
         setSeed(seed);
-      });
-    }
-  }, []);
+        });
+      }
+  },[]);
 
   const fetchEthData = async () => {
-    if (seed) {
+      if (seed) {
       const hdNode = HDNodeWallet.fromSeed(seed);
       const path = `m/44'/60'/${ethAccounts}'/0`;
       const wallet = hdNode.derivePath(path);
@@ -45,6 +66,8 @@ function WalletPage(): JSX.Element {
         privateKey: wallet.privateKey,
         publicKey: computeAddress(wallet.publicKey),
         ethTokens,
+        sendETHAmount: 0,
+        sendETHAddress: "",
       }]);
     }
   };
@@ -64,9 +87,11 @@ function WalletPage(): JSX.Element {
       });
       const solTokens = data.data.result.value / Math.pow(10, 9);
       setSolKeys([...solKeys, {
-        privateKey: encodeBase58(keypair.secretKey),
+        privateKey: bs58.encode(keypair.secretKey),
         publicKey: keypair.publicKey.toString(),
         solTokens,
+        sendSOLAmount: 0,
+        sendSOLAddress: "",
       }]);
     }
   };
@@ -87,12 +112,80 @@ function WalletPage(): JSX.Element {
     setethAccounts(ethAccounts + 1);
   };
 
-  const sendETH = async () => {
-
+  const setSOLamount = (index: number, amount: number) => {
+    const updatedSolKeys = [...solKeys];
+    updatedSolKeys[index].sendSOLAmount = amount;
+    setSolKeys(updatedSolKeys);
   };
 
-  const sendSOL = async () => {
+  const setSOLaddress = (index: number, address: string) => {
+    const updatedSolKeys = [...solKeys];
+    updatedSolKeys[index].sendSOLAddress = address;
+    setSolKeys(updatedSolKeys);
+    // console.log(`SOL address for ${index}: ${address}`)
   };
+
+  const setETHamount = (index: number, amount: number) => {
+    const updatedEthKeys = [...ethKeys];
+    updatedEthKeys[index].sendETHAmount = amount;
+    setEthKeys(updatedEthKeys);
+    // console.log(`ETH amount for ${index}: ${amount}`)
+  };
+
+  const setETHaddress = (index: number, address: string) => {
+    const updatedEthKeys = [...ethKeys];
+    updatedEthKeys[index].sendETHAddress = address;
+    setEthKeys(updatedEthKeys);
+    // console.log(`ETH address for ${index}: ${address}`)
+  };
+
+
+  const sendSOL = async (index: number) => {
+    if (solKeys[index]) {
+        const decodedPrivateKey = bs58.decode(solKeys[index].privateKey);
+        const from = Keypair.fromSecretKey(decodedPrivateKey);
+        const to = new PublicKey(solKeys[index].sendSOLAddress);
+        const amount = LAMPORTS_PER_SOL * solKeys[index].sendSOLAmount;
+  
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+        const { blockhash} = await connection.getLatestBlockhash();
+  
+        const transaction = new Transaction().add(SystemProgram.transfer({
+          fromPubkey: from.publicKey,
+          toPubkey: to,
+          lamports: amount
+        }));
+  
+        transaction.recentBlockhash = blockhash;
+        transaction.feePayer = from.publicKey;
+        transaction.sign(from);
+
+        const signature = await sendAndConfirmTransaction(connection, transaction, [from])
+        if(signature) {
+          alert("Transaction Successfull, signature: " + signature +"\n Reload the Website and generate Wallets again");
+        }
+    }
+  };
+
+  const sendETH = async (index: number) => {
+    const providerUrl = 'https://eth-holesky.g.alchemy.com/v2/atNVG8-veYm43Ng99fSUIFOixbOyPPZe'
+    const provider = new JsonRpcProvider(providerUrl);
+
+    const privateKey = ethKeys[index].privateKey;
+    const wallet = new Wallet(privateKey,provider)
+
+    const transaction = {
+      to : ethKeys[index].sendETHAddress,
+      value: parseEther(ethKeys[index].sendETHAmount.toString()),
+      gasLimit: 21000,
+      gasPrice: parseUnits('10','gwei')
+    }
+
+    const transactionResponse = await wallet.sendTransaction(transaction);
+    if(transactionResponse) {
+      alert('Transaction Complete! reload to see balance');
+    }
+  }
 
   return (
     <div className='min-h-screen h-auto w-full p-20 bg-slate-900'>
@@ -135,14 +228,14 @@ function WalletPage(): JSX.Element {
                   </div>
                   <input type="number"
                     className="m-2 input input-bordered w-full max-w-xs"
-                    value={sendSOLAmount}
-                    onChange={(e) => setSendSOLAmount(Number(e.target.value))} />
+                    value={solKeys[index].sendSOLAmount}
+                    onChange={(e) => setSOLamount(index, Number(e.target.value))} />
                   <input type="text"
                     placeholder="Enter Wallet Address"
                     className="m-2 input input-bordered w-full max-w-xs"
-                    value={sendSOLAddress}
-                    onChange={(e) => setSendSOLAddress(e.target.value)} />
-                  <button onClick={() => sendSOL()} className="btn btn-active">SEND SOL</button>
+                    value = {solKeys[index].sendSOLAddress}
+                    onChange= {(e) => setSOLaddress(index, e.target.value)} />
+                  <button onClick={() => sendSOL(index)} className="btn btn-active">SEND SOL</button>
                 </div>
               </div>
             )
@@ -182,14 +275,14 @@ function WalletPage(): JSX.Element {
                   </div>
                   <input type="number"
                     className="m-2 input input-bordered w-full max-w-xs"
-                    value={sendETHAmount}
-                    onChange={(e) => setSendETHAmount(Number(e.target.value))} />
-                  <input type="text"
+                    value={ethKeys[index].sendETHAmount}
+                    onChange={(e) => setETHamount(index, Number(e.target.value))} />
+                    <input type="text"
                     placeholder="Enter Wallet Address"
                     className="m-2 input input-bordered w-full max-w-xs"
-                    value={sendETHAddress}
-                    onChange={(e) => setSendETHAddress(e.target.value)} />
-                  <button onClick={() => sendETH()} className="btn btn-active">SEND ETH</button>
+                    value={ethKeys[index].sendETHAddress}
+                    onChange={(e) => setETHaddress(index, e.target.value)} />
+                  <button onClick={() => sendETH(index)} className="btn btn-active">SEND ETH</button>
                 </div>
               </div>
             )
